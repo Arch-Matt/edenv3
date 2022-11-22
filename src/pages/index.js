011f4656fa1842eedbaa043b8d1a6d66ff04bfee
Arch-Matt <matt@archcreative.co.uk>
2022-11-22 14:29:27 +0000

    Removed all client side

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,11 @@ import clsx from 'clsx';
 import Layout from '@theme/Layout';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import slickScript from '../js/slick.js';
+// import slickScript from '../js/slick.js';
 import $ from 'jquery';
 // import themeScript from '../js/theme.js';
 import styles from './index.module.css';
-import BrowserOnly from '@docusaurus/BrowserOnly';
+
 // import HomepageFeatures from '../components/HomepageFeatures';
 
 function HomepageHeader() {
@@ -311,107 +311,116 @@ export default function Home() {
   );
 }
 
-$(function(){
-  $(".home-slider").slick({
-    dots:false,
-    arrows:false,
-    autoplay:true,
-    autoplaySpeed:0,
-    speed:10000,
-    easing:'linear',
-    pauseOnHover:false,
-    slidesToShow:5,
-    cssEase:'linear',
-    centerMode:true,
-    centerPadding:'30px',
-    responsive: [
-      {
-        breakpoint: 990,
-        settings: {
-          slidesToShow: 4
-        }
-      },
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 3
-        }
-      },
-      {
-        breakpoint: 560,
-        settings: {
-          slidesToShow: 2
-        }
-      }
-    ]
-  });
-  $(".home-slider-rtl").slick({
-    dots:false,
-    arrows:false,
-    autoplay:true,
-    autoplaySpeed:0,
-    speed:10000,
-    easing:'linear',
-    pauseOnHover:false,
-    slidesToShow:5,
-    rtl:true,
-    cssEase:'linear',
-    responsive: [
-      {
-        breakpoint: 990,
-        settings: {
-          slidesToShow: 4
-        }
-      },
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 3
-        }
-      },
-      {
-        breakpoint: 560,
-        settings: {
-          slidesToShow: 2
-        }
-      }
-    ]
-  });
+// $(function(){
+//   $(".home-slider").slick({
+//     dots:false,
+//     arrows:false,
+//     autoplay:true,
+//     autoplaySpeed:0,
+//     speed:10000,
+//     easing:'linear',
+//     pauseOnHover:false,
+//     slidesToShow:5,
+//     cssEase:'linear',
+//     centerMode:true,
+//     centerPadding:'30px',
+//     responsive: [
+//       {
+//         breakpoint: 990,
+//         settings: {
+//           slidesToShow: 4
+//         }
+//       },
+//       {
+//         breakpoint: 768,
+//         settings: {
+//           slidesToShow: 3
+//         }
+//       },
+//       {
+//         breakpoint: 560,
+//         settings: {
+//           slidesToShow: 2
+//         }
+//       }
+//     ]
+//   });
+//   $(".home-slider-rtl").slick({
+//     dots:false,
+//     arrows:false,
+//     autoplay:true,
+//     autoplaySpeed:0,
+//     speed:10000,
+//     easing:'linear',
+//     pauseOnHover:false,
+//     slidesToShow:5,
+//     rtl:true,
+//     cssEase:'linear',
+//     responsive: [
+//       {
+//         breakpoint: 990,
+//         settings: {
+//           slidesToShow: 4
+//         }
+//       },
+//       {
+//         breakpoint: 768,
+//         settings: {
+//           slidesToShow: 3
+//         }
+//       },
+//       {
+//         breakpoint: 560,
+//         settings: {
+//           slidesToShow: 2
+//         }
+//       }
+//     ]
+//   });
 
-  $(".slider-twitter").slick({
-    dots:true,
-    arrows:false,
-    autoplay:false,
-    autoplaySpeed:0,
-    speed:300,
-    easing:'linear',
-    pauseOnHover:true,
-    slidesToShow:5,
-    cssEase:'linear',
-    responsive: [
-      {
-        breakpoint: 990,
-        settings: {
-          slidesToShow: 4
-        }
-      },
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 3
-        }
-      },
-      {
-        breakpoint: 560,
-        settings: {
-          slidesToShow: 2
-        }
-      }
-    ]
-  });
+//   $(".slider-twitter").slick({
+//     dots:true,
+//     arrows:false,
+//     autoplay:false,
+//     autoplaySpeed:0,
+//     speed:300,
+//     easing:'linear',
+//     pauseOnHover:true,
+//     slidesToShow:5,
+//     cssEase:'linear',
+//     responsive: [
+//       {
+//         breakpoint: 990,
+//         settings: {
+//           slidesToShow: 4
+//         }
+//       },
+//       {
+//         breakpoint: 768,
+//         settings: {
+//           slidesToShow: 3
+//         }
+//       },
+//       {
+//         breakpoint: 560,
+//         settings: {
+//           slidesToShow: 2
+//         }
+//       }
+//     ]
+//   });
 
-$("video").trigger("play");
+// $("video").trigger("play");
 
  
-});
+// });
 
+
+// window.onscroll = function(){
+//   var scrollTop = window.pageYOffset;
+//   if(scrollTop >= 1){
+//     $(".navbar").addClass("bg");
+//   }else{
+//     $(".navbar").removeClass("bg");
+//   }
+// };
