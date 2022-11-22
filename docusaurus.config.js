e382f3407e00833db196ec9eeec2e7f113c320bd
Arch-Matt <matt@archcreative.co.uk>
2022-11-22 12:55:22 +0000

    first commit

---
+++ docusaurus.config.js
@@ -0,0 +1,170 @@
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+// With JSDoc @type annotations, IDEs can provide config autocompletion
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
+(module.exports = {
+  title: 'EDEN Network',
+  tagline: 'EDEN',
+  url: 'https://Arch-Matt.github.io',
+  baseUrl: '/edenv3/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'Arch-Matt', // Usually your GitHub org/user name.
+  projectName: 'edenv3', // Usually your repo name.
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl: 'https://github.com/facebook/docusaurus/edit/main/website/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/main/website/blog/',
+        },
+        theme: {
+          customCss: [
+            require.resolve('./src/css/m-grid.2.0.0.css'),
+            require.resolve('./src/css/slick.css'),
+            require.resolve('./src/css/slick-theme.css'),
+            require.resolve('./src/css/custom.css')
+          ],
+        },
+      }),
+    ],
+  ],
+ 
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    
+    ({
+      navbar: {
+        logo: {
+          alt: 'EDEN Logo',
+          src: 'img/eden-logo-white.svg',
+        },
+        items: [
+          {
+            type: 'doc', 
+            docId: 'intro',
+            position: 'left',
+            label: 'Docs',
+          },
+          {to: '/blog', label: 'Blog', position: 'left'},
+          {
+            href: 'https://google.co.uk',
+            label: 'Launch App',
+            position: 'right',
+          },
+        ],
+      },
+      colorMode:{
+        disableSwitch:true,
+      },
+      footer: {
+        style: 'dark',
+        logo:{
+          src: 'static/img/eden-logo-white.svg',
+        },
+        links: [
+          {
+            title: 'Products',
+            items: [
+              {
+                label: 'Eden Relay',
+                to: '/docs/intro',
+              },
+              {
+                label: 'Eden RPC',
+                to: '/docs/intro',
+              },
+              {
+                label: 'EDEN Token',
+                to: '/docs/intro',
+              },
+              {
+                label: 'yyAVAX on Yield Yak',
+                to: '/docs/intro',
+              },
+            ],
+          },
+          {
+            title: 'Developers',
+            items: [
+              {
+                label: 'Github',
+                to: '/docs/intro',
+              },
+              {
+                label: 'Docs',
+                to: '/docs/intro',
+              },
+            ],
+          },
+          {
+            title: 'Company',
+            items: [
+              {
+                label: 'Eden Network',
+                to: '/docs/intro',
+              },
+              {
+                label: 'Linkedin',
+                to: '/docs/intro',
+              },
+              {
+                label: 'Brand Assets',
+                to: '/docs/intro',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Twitter',
+                to: '/docs/intro',
+              },
+              {
+                label: 'Discord',
+                to: '/docs/intro',
+              },
+              {
+                label: 'Medium',
+                to: '/docs/intro',
+              },
+              {
+                label: 'Telegram',
+                to: '/docs/intro',
+              },
+            ],
+          },
+          {
+            title: 'Legal',
+            items: [
+              {
+                label: 'Terms',
+                to: '/docs/intro',
+              },
+              {
+                label: 'Privacy',
+                to: '/docs/intro',
+              },
+            ],
+          },
+        ],
+        copyright: `© Eden Network ${new Date().getFullYear()}.`,
+      },
+      prism: {
+        theme: darkCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+});
---
+++ src/pages/index.js
@@ -0,0 +1,425 @@
+import React from 'react';
+import clsx from 'clsx';
+import Layout from '@theme/Layout';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import slickScript from '../js/slick.js';
+import $ from 'jquery';
+// import themeScript from '../js/theme.js';
+import styles from './index.module.css';
+// import HomepageFeatures from '../components/HomepageFeatures';
+
+function HomepageHeader() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <header className="header-home">
+        <video muted="muted" loop>
+            <source src="/static/vid/eden-3.mp4"></source>
+          </video>
+        <div className="header-content">
+          <div className="m-container-1400">
+            <h1>Multichain Infrastructure for <span className="text-green">Maximal Value</span></h1>
+            <div className="logo-list">
+              <div><img src="static/img/logo-jumpcap.svg" alt=""/></div>
+              <div><img src="static/img/logo-multicoin.svg" alt=""/></div>
+              <div><img src="static/img/logo-gsr.svg" alt=""/></div>
+              <div><img src="static/img/logo-wintermute.svg" alt=""/></div>
+            </div>
+            <div className="white-boxes">
+              <div className="m-grid m-grid-gap-30">
+                <div className="m-xl-6 m-lg-6 m-md-12 m-sm-12 m-xs-12 white-box">
+                  <img className="sub-logo" src="static/img/eden-logo-blue-rpc.svg" alt=""/>
+                  <img className="available-on" src="static/img/available-on-eth.svg" alt="" />
+                  <h2>MEV Protection for Traders</h2>
+                  <p>Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.</p>
+                  <div className="lower-links">
+                    <a href="" className="button">Get started</a>
+                    <a href="" className="docs"></a>
+                  </div>
+                </div>
+                <div className="m-xl-6 m-lg-6 m-md-12 m-sm-12 m-xs-12 white-box">
+                  <img className="sub-logo" src="static/img/eden-logo-blue-relay.svg" alt=""/>
+                  <img className="available-on" src="static/img/available-on-eth.svg" alt="" />
+                  <h2>MEV Extraction for Web3 Infrastructure</h2>
+                  <p>Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.</p>
+                  <div className="lower-links">
+                    <a href="" className="button">Get started</a>
+                    <a href="" className="docs"></a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+    </header>
+  );
+}
+
+
+
+
+
+function HomepageNumbers() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <div className="module-numbers">
+      <div className="m-container-1400">
+        <div className="m-grid numbers-grid">
+          <div className="m-xl-3 m-lg-3 m-md-6 m-sm-6 m-sm-6 m-xs-12 number-grid-col">
+            <span className="number-large">9,321</span>
+            <span className="number-label">Validators</span>
+          </div>
+          <div className="m-xl-3 m-lg-3 m-md-6 m-sm-6 m-sm-6 m-xs-12 number-grid-col">
+            <span className="number-large">#1</span>
+            <span className="number-label">Block Profit Rank</span>
+          </div>
+          <div className="m-xl-3 m-lg-3 m-md-6 m-sm-6 m-sm-6 m-xs-12 number-grid-col">
+            <span className="number-large">$1,000</span>
+            <span className="number-label">MEV Extracted</span>
+          </div>
+          <div className="m-xl-3 m-lg-3 m-md-6 m-sm-6 m-sm-6 m-xs-12 number-grid-col">
+            <span className="number-large">1.55<span className="number-eth">ETH</span></span>
+            <span className="number-label">Average Block Reward</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+
+function HomepageLogoSliders() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <div className="module-logosliders">
+      <div className="m-container-1400 text-center">
+        <h2>Maximising profits for the best in Web3</h2>
+        <div className="sliderholder">
+          <div className="home-slider">
+            <div className="slide">
+              <img src="static/img/logo-binance.svg" alt=""/>
+            </div>
+            <div className="slide">
+              <img src="static/img/logo-bitcoin_suisse.svg" alt=""/>
+            </div>
+            <div className="slide">
+              <img src="static/img/logo-bitstamp.svg" alt=""/>
+            </div>
+            <div className="slide">
+              <img src="static/img/logo-chorus.svg" alt=""/>
+            </div>
+            <div className="slide">
+              <img src="static/img/logo-coinbase.svg" alt=""/>
+            </div>
+            <div className="slide">
+              <img src="static/img/logo-huobi.svg" alt=""/>
+            </div>
+          </div>
+        </div>
+        <div className="sliderholder" dir="rtl">
+          <div className="home-slider-rtl" >
+            <div className="slide">
+              <img src="static/img/logo-staked.svg" alt=""/>
+            </div>
+            <div className="slide">
+              <img src="static/img/logo-rocket.svg" alt=""/>
+            </div>
+            <div className="slide">
+              <img src="static/img/logo-solana.svg" alt=""/>
+            </div>
+            <div className="slide">
+              <img src="static/img/logo-s.svg" alt=""/>
+            </div>
+            <div className="slide">
+              <img src="static/img/logo-kraken.svg" alt=""/>
+            </div>
+            <div className="slide">
+        
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+
+
+function HomepageQuote() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <div className="module-quote">
+      <div className="m-container-1400">
+        <div className="m-grid">
+          <div className="m-xl-3 m-lg-3 m-md-4 m-sm-3 m-xs-12">
+            <div className="text-center vertical-align">
+              <div className="vertical-outer">
+                <div className="vertical-inner">
+                <img className="quote-image" src="static/img/near-protocol-near-logo.svg" alt="" />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="m-xl-1 m-lg-1 m-md-1 m-sm-1 m-xs-0"></div>
+          <div className="m-xl-8 m-lg-8 m-md-7 m-sm-8 m-xs-12">
+            <p className="quote">“Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo.”</p>
+            <p className="quote-by">Important Person. Important Company</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function HomepageTweets() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <div className="module-logosliders">
+      <div className="m-container-1400 text-center">
+        <h2>Latest news from our world</h2>
+        <div className="twitter-slider">
+          <div className="slider-twitter" >
+
+            <div className="tweet-slide">
+              <div class="tweet-slide-box">
+                <div class="tweet-author">
+                  <img src="static/img/twitter-pic.png" alt="" />
+                  <span class="tweet-author-name">John Smith</span>
+                  <span class="tweet-author-handle">@john_smith</span>
+                </div>
+                <div class="tweet">
+                  <p>Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo.</p>
+                </div>
+              </div>
+            </div>
+
+
+            <div className="tweet-slide">
+              <div class="tweet-slide-box">
+                <div class="tweet-author">
+                  <img src="static/img/twitter-pic.png" alt="" />
+                  <span class="tweet-author-name">John Smith</span>
+                  <span class="tweet-author-handle">@john_smith</span>
+                </div>
+                <div class="tweet">
+                  <p>Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo.</p>
+                </div>
+              </div>
+            </div>
+
+
+            <div className="tweet-slide">
+              <div class="tweet-slide-box">
+                <div class="tweet-author">
+                  <img src="static/img/twitter-pic.png" alt="" />
+                  <span class="tweet-author-name">John Smith</span>
+                  <span class="tweet-author-handle">@john_smith</span>
+                </div>
+                <div class="tweet">
+                  <p>Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo.</p>
+                </div>
+              </div>
+            </div>
+
+
+            <div className="tweet-slide">
+              <div class="tweet-slide-box">
+                <div class="tweet-author">
+                  <img src="static/img/twitter-pic.png" alt="" />
+                  <span class="tweet-author-name">John Smith</span>
+                  <span class="tweet-author-handle">@john_smith</span>
+                </div>
+                <div class="tweet">
+                  <p>Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo.</p>
+                </div>
+              </div>
+            </div>
+
+            <div className="tweet-slide">
+              <div class="tweet-slide-box">
+                <div class="tweet-author">
+                  <img src="static/img/twitter-pic.png" alt="" />
+                  <span class="tweet-author-name">John Smith</span>
+                  <span class="tweet-author-handle">@john_smith</span>
+                </div>
+                <div class="tweet">
+                  <p>Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo.</p>
+                </div>
+              </div>
+            </div>
+
+            <div className="tweet-slide">
+              <div class="tweet-slide-box">
+                <div class="tweet-author">
+                  <img src="static/img/twitter-pic.png" alt="" />
+                  <span class="tweet-author-name">John Smith</span>
+                  <span class="tweet-author-handle">@john_smith</span>
+                </div>
+                <div class="tweet">
+                  <p>Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo.</p>
+                </div>
+              </div>
+            </div>
+
+
+
+
+
+
+
+
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+
+function HomepageCTA() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <div className="module-cta">
+      <div className="m-container-1400 text-center">
+        <h2 className="large text-blue">Ready to maximise your projects earnings?</h2>
+        <a href="" className="button blue large">Contact us now</a>
+      </div>
+    </div>
+  );
+}
+
+
+
+export default function Home() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <Layout
+      title={`${siteConfig.title}`}
+      description="Description will go into a meta tag in <head />">
+      <HomepageHeader />
+      <main>
+        
+        <HomepageNumbers />
+        <HomepageLogoSliders />
+        <HomepageQuote />
+        <HomepageTweets />
+        <HomepageCTA />
+      
+      </main>
+    </Layout>
+  );
+}
+
+$(function(){
+  $(".home-slider").slick({
+    dots:false,
+    arrows:false,
+    autoplay:true,
+    autoplaySpeed:0,
+    speed:10000,
+    easing:'linear',
+    pauseOnHover:false,
+    slidesToShow:5,
+    cssEase:'linear',
+    centerMode:true,
+    centerPadding:'30px',
+    responsive: [
+      {
+        breakpoint: 990,
+        settings: {
+          slidesToShow: 4
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 560,
+        settings: {
+          slidesToShow: 2
+        }
+      }
+    ]
+  });
+  $(".home-slider-rtl").slick({
+    dots:false,
+    arrows:false,
+    autoplay:true,
+    autoplaySpeed:0,
+    speed:10000,
+    easing:'linear',
+    pauseOnHover:false,
+    slidesToShow:5,
+    rtl:true,
+    cssEase:'linear',
+    responsive: [
+      {
+        breakpoint: 990,
+        settings: {
+          slidesToShow: 4
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 560,
+        settings: {
+          slidesToShow: 2
+        }
+      }
+    ]
+  });
+
+  $(".slider-twitter").slick({
+    dots:true,
+    arrows:false,
+    autoplay:false,
+    autoplaySpeed:0,
+    speed:300,
+    easing:'linear',
+    pauseOnHover:true,
+    slidesToShow:5,
+    cssEase:'linear',
+    responsive: [
+      {
+        breakpoint: 990,
+        settings: {
+          slidesToShow: 4
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 560,
+        settings: {
+          slidesToShow: 2
+        }
+      }
+    ]
+  });
+
+$("video").trigger("play");
+
+ 
+});
+
+
+window.onscroll = function(){
+  var scrollTop = window.pageYOffset;
+  if(scrollTop >= 1){
+    $(".navbar").addClass("bg");
+  }else{
+    $(".navbar").removeClass("bg");
+  }
+};
